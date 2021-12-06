c15bd42638bf3d75d8eea98a8c5bb5e54858b61f
https://github.com/vter943/QA11_firstproject <vter943@gmail.com>
2021-12-06 16:32:57 +0100

    detailed report fnctnlty added

--- src/test/java/vdtask/ApplicationManager.java
+++ src/test/java/vdtask/ApplicationManager.java
@@ -1,16 +1,49 @@
 package vdtask;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
-public class ApplicationManager  {
+public class ApplicationManager {
 
-    WebDriver wd;
+    EventFiringWebDriver wd;
     MainPageHelper mainPage;
     MarketItemHelper marketItem;
     MarketHeaderHelper marketHelper;
+
+
+    public static class MyListener extends AbstractWebDriverEventListener {
+
+        Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+
+
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            logger.info("Start search " +by);
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            logger.info(by + " found.");
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            logger.error(throwable.toString());
+        }
+    }
+
+    public ApplicationManager() {
+        this.wd = wd;
+    }
 
     public MainPageHelper getMainPage() {
         return mainPage;
@@ -25,7 +58,7 @@ public class ApplicationManager  {
     }
 
     public void init() {
-        wd = new ChromeDriver();
+        wd = new EventFiringWebDriver(new ChromeDriver());
         wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         wd.manage().window().maximize();
         openSite("https://yandex.ru/");
@@ -33,7 +66,9 @@ public class ApplicationManager  {
         mainPage = new MainPageHelper(wd);
         marketItem = new MarketItemHelper(wd);
         marketHelper = new MarketHeaderHelper(wd);
+        wd.register(new MyListener());
     }
+
     public void openSite(String url) {
         wd.navigate().to(url);
     }
@@ -41,7 +76,6 @@ public class ApplicationManager  {
     public void stop() {
         wd.quit();
     }
-
 
 
 }
